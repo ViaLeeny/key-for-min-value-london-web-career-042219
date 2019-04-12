caaf804bffa4e1d629babd897d39cc2b982d7d83
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-12 22:55:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash.find do |key, value|
-  if 
-  
-end
+
+  name_hash.values.min
+
 end
